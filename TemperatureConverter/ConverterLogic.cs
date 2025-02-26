3a9bff60b9cf86f71e2c72440b1c8f5a1ebd6714
Shadvonic <moxeymarcus28@gmail.com>
2025-02-26 15:29:22 -0500

    Add project files.

---
+++ TemperatureConverter/ConverterLogic.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperatureConverter
+{
+    public static class ConverterLogic
+    {
+        // Conversion of Temperature Between Celsius and Kelvin
+        public static double ConvertCelsiusToKelvin(double tempInCelsius)
+        {
+            double kelvin = tempInCelsius + 273.15;
+           
+            Console.WriteLine($"{Math.Round(tempInCelsius,1)}F is {Math.Round(kelvin, 1)}K");
+            return kelvin;
+        }
+
+
+        public static double ConvertKelvinToCelsius(double tempInKelvin)
+        {
+            double celsius = tempInKelvin - 273.15;
+          
+            Console.WriteLine($"{Math.Round(tempInKelvin,1)}K is {Math.Round(celsius, 1)}C");
+            return celsius;
+        }
+
+
+        // Conversion of Temperature Between Fahrenheit and Celsius
+        public static double ConvertFahrenheitToCelsius(double tempInFahrenheit)
+        {
+
+            double celsius = (tempInFahrenheit - 32) * (5 / 9);
+        
+            Console.WriteLine($"{Math.Round(tempInFahrenheit, 1)}F is {Math.Round(celsius, 1)}C");
+
+            return celsius;
+        }
+
+
+        public static double ConvertCelsiusToFahrenheit(double tempInCelsius)
+        {
+            double fahrenheit = (tempInCelsius * (9 / 5)) + 32;
+            
+            Console.WriteLine($"{Math.Round(tempInCelsius, 1)}C is {Math.Round(fahrenheit, 1)}C");
+            return fahrenheit;
+        }
+
+
+        //Conversion of Temperature Between Fahrenheit and Kelvin
+        public static double ConvertFahrenheitToKelvin(double tempInFahrenheit)
+        {
+            double kelvin = (tempInFahrenheit - 32) * (5 / 9) + 273.15;
+        
+            Console.WriteLine($"{Math.Round(tempInFahrenheit, 1)}F is {Math.Round(kelvin, 1)}F");
+            return kelvin;
+        }
+
+
+        public static double ConvertKelvinToFahrenheit(double tempInKelvin)
+        {
+            double fahrenheit = (tempInKelvin - 273.15) * (9/5) + 32;
+            
+            Console.WriteLine($"{Math.Round(tempInKelvin, 1)}K is {Math.Round(fahrenheit, 1)}F");
+            return fahrenheit;
+        }
+    }
+}
---
+++ TemperatureConverter/Program.cs
@@ -0,0 +1,43 @@
+﻿
+
+
+/*
+ * 
+ * Build a program that converts temperatures between Celsius, Fahrenheit, and Kelvin. Use
+ * breakpoints to debug the conversion logic.
+ * 
+ * Steps:
+ * Write functions to convert between the three temperature scales.
+ * Set breakpoints to inspect the input and output values of each conversion function.
+ * Debug edge cases, such as extremely high or low temperatures.
+ * 
+ */
+
+using TemperatureConverter;
+
+double temp;
+
+do
+{
+	Console.Write("Enter a tempature: ");
+	string userTemp = Console.ReadLine();
+	if (!double.TryParse(userTemp, out temp))
+	{
+		Console.WriteLine("Please enter a valid temp");
+	}
+    else
+    {
+        ConverterLogic.ConvertCelsiusToKelvin(temp);
+        ConverterLogic.ConvertKelvinToCelsius(temp);
+        ConverterLogic.ConvertFahrenheitToCelsius(temp);
+        ConverterLogic.ConvertCelsiusToFahrenheit(temp);
+        ConverterLogic.ConvertFahrenheitToKelvin(temp);
+        ConverterLogic.ConvertKelvinToFahrenheit(temp);
+    }
+
+    
+
+} while (temp != null); 
+
+
+
